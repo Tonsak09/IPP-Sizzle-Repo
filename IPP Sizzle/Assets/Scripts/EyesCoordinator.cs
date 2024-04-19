de305f37dcc6fbd9a62e6a7b3ecf9393be975b0f
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2024-04-18 20:01:10 -0400

    Eyes

---
+++ IPP Sizzle/Assets/Scripts/EyesCoordinator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesCoordinator : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] List<Renderer> eyeRenderers;
+    [SerializeField] Vector3 eyesCenterOffset;
+    [SerializeField] Transform eyesCenter;
+
+    private Material eyeMaterial;
+    float mag;
+
+    private void Start()
+    {
+        eyeMaterial = eyeRenderers[0].material;
+        mag = eyeMaterial.GetVector("_PupilOrigin").magnitude;
+    }
+
+    void Update()
+    {
+        foreach(Renderer r in eyeRenderers)
+        {
+            ProcessEye(r);
+        }
+    }
+
+    private void ProcessEye(Renderer eye)
+    {
+        Vector3 dir = eye.transform.InverseTransformDirection(target.position - eye.transform.position).normalized;
+        eye.material.SetVector("_PupilOrigin", dir * mag);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(eyesCenter.position + eyesCenterOffset, 0.05f);
+    }
+}
